460c98e2a13f902853d3919efdac6b826c779d45
Anthony Riley <anth1y@gmail.com>
2014-08-30 12:33:13 -0700

    Preventing duplicate entries

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,5 @@
 class Bar < ActiveRecord::Base
+  validates :title, :body, :authors, :entry_url, :description, :published_at, presence: true
+  validates :title, uniqueness: true
+  validates :entry_url, uniqueness: true
 end
